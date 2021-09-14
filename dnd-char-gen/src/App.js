d0b3699428b18ea8f535347dc033d64520f8394a
tburto33 <tburto2387@gmail.com>
2021-09-14 19:12:21 -0400

    save point!

--- dnd-char-gen/src/App.js
+++ dnd-char-gen/src/App.js
@@ -1,5 +1,8 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
   // const [currentTime, setCurrentTime] = useState(0);
@@ -8,6 +11,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button>Test Button</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
