cafbb3384dabec8c36b64045457a91049d4a9d1b
tburto33 <tburto2387@gmail.com>
2021-09-18 20:28:30 -0400

    got react-bootstrap working and starting framing site

--- dnd-char-gen/src/App.js
+++ dnd-char-gen/src/App.js
@@ -1,29 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-// import { Button } from 'react-bootstrap';
-
+import dndheader from './dnd.png';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from 'react-bootstrap/Button';
 
 function App() {
-  // const [currentTime, setCurrentTime] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <Button>Test Button</Button> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is 0.</p>
+        <div class="col">
+          <h1>ONESHOT</h1>
+          <div class="col-sm text-center">
+            <h6>A CHARACTER GENERATOR FOR</h6>
+          </div>
+          <div>
+            <img class="img-fluid header-img" src={dndheader} alt="dnd-logo" />
+          </div>
+          <div class="col-sm text-center">
+            <Button variant="primary">REROLL</Button>
+          </div>
+        </div>
       </header>
+      <body>
+        <p>Hello World!</p>
+      </body>
     </div>
   );
 }
